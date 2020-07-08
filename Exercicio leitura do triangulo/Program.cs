a01e658b0c17429cb565568794bcb50e78412950
Leandro Sousa <leandroscjTI@gmail.com>
2020-07-08 01:15:25 -0300

    Add project files.

---
+++ Exercicio leitura do triangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio_leitura_do_triangulo
+{
+    static class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Triangulo ret = new Triangulo();
+
+            Console.WriteLine("Digite a largura do Triangulo:");
+            ret.Largura = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite a altura do Triangulo:");
+            ret.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area = " + ret.Area().ToString("F2"));
+            Console.WriteLine("Perimetro = " + ret.Perimetro().ToString("f2"));
+            Console.WriteLine("Diagonal = " + ret.Diagonal().ToString("F2"));
+
+        }
+    }
+}
---
+++ Exercicio leitura do triangulo/Triangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio_leitura_do_triangulo
+{
+    class Triangulo
+    {
+        public double Altura;
+        public double Largura;
+
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+        public double Diagonal()
+        {
+
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+
+
+        }
+    }
+}
